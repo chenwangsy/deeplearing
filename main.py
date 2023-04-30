9d6637740b2e019925628b5d47d414f3d2d89770
陈王双懿 <70421379+chenwangsy@users.noreply.github.com>
2023-04-30 20:29:55 +0800

    hooks.onecyclelrupdatehook学习笔记
    hooks.onecyclelrupdatehook学习笔记

---
+++ main.py
@@ -0,0 +1,31 @@
+class A:
+	def __init__(self):
+		pass
+
+	def forward(self, ):
+		print("A haha")
+		return 100
+
+class B(A):
+	def __init__(self):
+		pass
+
+	def forward(self, ):
+		print("B haha")
+
+
+
+a = []
+print(a.__class__)
+
+# a2 = A()
+# b = B()
+# a_forward = getattr(A, 'forward')
+# a2_forward = getattr(A, 'forward')
+#
+# print(a_forward)
+# print(a2_forward)
+# b_forward = getattr(B, 'forward')
+# print(b_forward)
+#
+# print(a_forward == b_forward)
